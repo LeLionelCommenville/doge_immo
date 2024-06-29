b15b4b46ff341aa1d13c9afca562c2bc22f919dd
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-06-29 18:36:40 +0200

    feat: add a property controller

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PropertyController extends AbstractController
+{
+    #[Route('/property', name: 'app_property')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/PropertyController.php',
+        ]);
+    }
+}
